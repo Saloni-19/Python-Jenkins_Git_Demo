5b4006a9c55f7afb0763abb19e6aae05fb4dd927
Saloni-19 <Salonikhandelwal920@gmail.com>
2021-02-10 13:08:05 -0800

    Second commit

---
+++ DictDemo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 14:50:29 2021
+
+@author: SaloniKhandelwal
+
+key-value pair
+"""
+
+dict1 = {"company":"IBM","type":"Elearing","year":2021,25.778:209.89}
+
+print(dict1["company"])      #IBM
+
+print(dict1.keys())          #dict_keys(['company', 'type', 'year'])
+
+print(dict1.values())        #dict_values(['IBM', 'Elearing', 2021])
+
